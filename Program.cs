ba9e7e4513fd1bbbedf9c43e408dc3529c6abb9d
Sylvain Tenier <sylvain_at_tenier_dot_name>
2017-09-18 18:34:15 +0200

    license

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿// Copyright (C) 2017 Sylvain TENIER
+// 
+// This file is part of MOSWdeploy.
+// 
+// MOSWdeploy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// MOSWdeploy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MOSWdeploy.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
 using System.Xml;
 using System.Xml.Linq;
 using System.Text;
@@ -12,22 +30,22 @@ namespace MOSWdeploy
         {
             string pathBase = Path.Combine("Files", "ReadOnly", @"FileZillaBase.xml");
             string pathOut = Path.Combine("Files", @"FileZilla Server.xml");
-            string baseDirFTP= @"c:\projetsweb\";
+            string baseDirFTP = @"c:\projetsweb\";
             string userTemplate = Path.Combine("Files", "ReadOnly", @"FZConfUserTemplate.xml");
-            string mySQLOutDir="Files";
+            string mySQLOutDir = "Files";
 
             // load existing groups
-            StudentGroup gr10= StudentGroup.LoadFromFile("gr10");
-            StudentGroup gr12= StudentGroup.LoadFromFile("gr12");
+            StudentGroup gr10 = StudentGroup.LoadFromFile("gr10");
+            StudentGroup gr12 = StudentGroup.LoadFromFile("gr12");
 
-            // groups creation
-//            StudentGroup gr12=new StudentGroup("gr12",15);
+            // new groups creation
+            // StudentGroup gr12=new StudentGroup("gr12",15);
 
             // create file backup
-//            gr12.SaveToFile();
+            // gr12.SaveToFile();
 
             // FileZilla generation
-            FileZillaConf fzConf=new FileZillaConf(pathBase,pathOut,baseDirFTP,userTemplate);
+            FileZillaConf fzConf = new FileZillaConf(pathBase, pathOut, baseDirFTP, userTemplate);
             fzConf.AddGroup(gr10);
             fzConf.AddGroup(gr12);
 
@@ -36,7 +54,7 @@ namespace MOSWdeploy
             fzConf.WriteXML();
 
             // MySQL generation
-            MySQLConf.GenerateDBFile(gr10,mySQLOutDir);
+            MySQLConf.GenerateDBFile(gr10, mySQLOutDir);
         }
     }
 }
